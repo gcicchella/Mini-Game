3f3e169b67dae495a22a94aae8bdea0b3b6982fc
g.cicchella <giuseppecicchella28@gmail.com>
2022-12-19 22:32:21 +0100

    Function random

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,27 @@
 import {Text, View, StyleSheet} from "react-native";
+import {useState} from "react";
 
-import Title from "../components/Title";
-function GameScreen() {
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+
+function generateRandomBetween(min, max, exclude){
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if(rndNum === exclude){
+        return generateRandomBetween(min, max, exclude);
+    }
+    else {
+        return rndNum;
+    }
+}
+function GameScreen({userNumber}) {
+    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const [currentGuess, setCurrentGues] = useState(initialGuess);
+
     return (
         <View style={styles.screen}>
             <Title>Ho pensato al numero</Title>
+            <NumberContainer>{currentGuess}</NumberContainer>
                 <View>
                     <Text>Più grande o più piccolo?</Text>
                 </View>
