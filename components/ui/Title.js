8002c1e81d63d889c731f406efcd650ecb0a2cf9
g.cicchella <giuseppecicchella28@gmail.com>
2022-12-20 15:30:48 +0100

    migliorata interfaccia utente orizzontale GAMEOVER

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text} from "react-native";
+import {StyleSheet, Text, Platform} from "react-native";
 import Colors from "../../constants/colors";
 function Title({children}){
     return(
